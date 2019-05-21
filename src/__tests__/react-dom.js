8f2ab9c905dfbc8e1adcd804eb7f79d9f01f7612
Michael Prouty <michael.prouty@gmail.com>
2019-05-21 14:06:23 -0400

    Complete react-dom.js

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,26 +1,14 @@
 // 🐨 you're going to need to use React to create react elements, so import react
 // 🐨 we're going to render the FavoriteNumber component with ReactDOM so you'll need to import react-dom
 // 🐨 Here's your component:
-// import {FavoriteNumber} from '../favorite-number'
+import 'jest-dom/extend-expect'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
-  // 🐨 create a div (💯 document.createElement)
-  // 🐨 render the FavoriteNumber component to that div with ReactDOM.render
-  // 🐨 assert the input type attribute is a number
-  //:🐨 assert the label's text content is "Favorite Number"
+  const div = document.createElement('div')
+  ReactDOM.render(<FavoriteNumber />, div)
+  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
+  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
 })
-
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=react-testing-library-course&e=react-dom&em=
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
-})
-////////////////////////////////
